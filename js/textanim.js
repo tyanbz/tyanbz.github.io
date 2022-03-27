a86ecfc167f92cc049f3c3ad8995b73f06531c43
eugene <etyan@protonmail.com>
2022-03-27 16:52:18 +0300

    add new prjcts

--- js/textanim.js
+++ js/textanim.js
@@ -3,7 +3,7 @@ window.onload = play();
 function play() {
   let typed = new Typed('#typed', { // Тут id того блока, в которм будет анимация
     stringsElement: '#typed-strings', // Тут id блока из которого берем строки для анимации
-    typeSpeed: 100, // Скорость печати
+    typeSpeed: 50, // Скорость печати
     startDelay: 500, // Задержка перед стартом анимации
     backSpeed: 50, // Скорость удаления
     loop: false // Указываем, повторять ли анимацию
